b3622f95ade53532a5773161de915debfd109d4a
Loic Baron <loic.baron@lip6.fr>
2017-10-22 14:42:22 +0200

    24h recall list of items and total

--- resources/views/24h.blade.php
+++ resources/views/24h.blade.php
@@ -7,16 +7,6 @@
 @section('head')
     @parent
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="/js/typeahead.bundle.js"></script>
-    <link rel="stylesheet" href="/css/typeahead.css">
-    <script type="text/javascript">
-    function isInt(n){
-        return Number(n) === n && n % 1 === 0;
-    }
-    function isFloat(n){
-        return Number(n) === n && n % 1 !== 0;
-    }
-    </script>
 
 @endsection
 
@@ -25,10 +15,49 @@
 </div>
 <div class="container-fluid col-md-12">
     <div class="row">
-    24h recall
+
+        <div class="panel panel-default" id="recall">
+            <div class="panel-heading" id="name" style="display:inline-block; width:100%;">
+            <h2>24h recall</h2>
+            </div>
+
+            <div class="table-responsive" id="list">
+            </div>
+        </div>
     </div>
 </div>
 <script type="text/javascript">
+$(document).ready(function(){
+    var h24 = localStorage.getItem('h24');
+    if(h24!=null) {
+        var oH24 = JSON.parse(h24);
+        $("#list").html(listItems);
+        var listItems = "<table class='table' style='text-align:right;'>";
+        $.each(oH24, function(i, val){
+            listItems += "<tr>";
+            listItems += "<td style='text-align:left;'>"+Object.keys(val)[0]+"</td>";
+            var values = val[Object.keys(val)[0]];
+            $.each(values, function(i, v){
+                listItems += "<td>"+v+"</td>";
+            });
+            listItems += "</tr>";
+        }); 
+        var total = localStorage.getItem("total");
+        if(total!=null) {
+            var oTotal = JSON.parse(total);
+            listItems += "<tr class='success' style='font-weight:bold;'><td style='text-align:left;'>Total</td>"
+            $.each(oTotal, function(i, val){
+                listItems += "<td>"+parseFloat(Math.round(val * 100) / 100).toFixed(2);+"</td>";
+            });
+            listItems += "</tr>";
+        }
+        listItems += "</table>";
+        $("#list").html(listItems);
+    }else{
+        $("#list").html("no food item selected");
+    }
+});
+
 </script>
 @endsection
 
