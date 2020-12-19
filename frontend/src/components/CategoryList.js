16dbca73731ddfdd57f9ca5f52d801413f7ff98e
Loic Baron <loic.baron@gmail.com>
2020-12-19 23:37:27 +0000

    frontend(category): add style

--- frontend/src/components/CategoryList.js
+++ frontend/src/components/CategoryList.js
@@ -3,13 +3,14 @@ import PropTypes from 'prop-types';
 import { Typography } from '@material-ui/core';
 import Category from '../models/Category';
 import FoodListContainer from '../containers/FoodListContainer';
+import './CategoryList.css';
 
 const CategoryList = ({ className, categories }) => (
   <div className={className}>
     {
     categories.map(category => (
       <div key={category.id} className={className}>
-        <Typography gutterBottom variant="h5" component="h2" key={category.name}>
+        <Typography gutterBottom variant="h4" component="h4" key={category.name} className="food-category">
           {category.name}
         </Typography>
         <FoodListContainer key={category.id} categoryId={category.id} />
