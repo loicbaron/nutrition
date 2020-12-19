5063a96a0777c694b8a199345a6a096950a8d473
Loic Baron <loic.baron@gmail.com>
2020-12-19 23:43:55 +0000

    fix(frontend): align Slider with Pictures

--- frontend/src/components/QuantitySlider.js
+++ frontend/src/components/QuantitySlider.js
@@ -1,40 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Slider } from '@material-ui/core';
-import Food from '../models/Food';
 
 const marks = [
   {
     value: 0,
     label: '0',
+    width: '100%',
   },
   {
     value: 1,
     label: 'A',
+    width: '36%',
   },
   {
     value: 2,
     label: 'B',
+    width: '54%',
   },
   {
     value: 3,
     label: 'C',
+    width: '65%',
   },
   {
     value: 4,
     label: 'D',
+    width: '72%',
   },
   {
     value: 5,
     label: 'E',
+    width: '78%',
   },
   {
     value: 6,
     label: 'F',
+    width: '81%',
   },
   {
     value: 7,
     label: 'G',
+    width: '85%',
   },
 ];
 function valuetext(value) {
@@ -46,27 +53,29 @@ function valueLabelFormat(value) {
 }
 const QuantitySlider = (props) => {
   const {
-    keys, item, onSelect, currentQuantity,
+    keys, onSelect, currentQuantity,
   } = props;
   const myMarks = marks.filter(mark => keys.includes(mark.label));
+  const width = [...myMarks].pop().width;
   return (
-    <Slider
-      value={currentQuantity}
-      getAriaValueText={valuetext}
-      valueLabelFormat={valueLabelFormat}
-      aria-labelledby="discrete-slider"
-      valueLabelDisplay="auto"
-      step={1}
-      marks={myMarks}
-      min={0}
-      max={myMarks.length - 1}
-      onChangeCommitted={(ev, val) => onSelect(item.id, val)}
-    />
+    <div style={{'width': width}}>
+      <Slider
+        value={currentQuantity}
+        getAriaValueText={valuetext}
+        valueLabelFormat={valueLabelFormat}
+        aria-labelledby="discrete-slider"
+        valueLabelDisplay="auto"
+        step={1}
+        marks={myMarks}
+        min={0}
+        max={myMarks.length}
+        onChangeCommitted={(ev, val) => onSelect(val)}
+      />
+    </div>
   );
 };
 
 QuantitySlider.propTypes = {
-  item: Food.shape.isRequired,
   keys: PropTypes.arrayOf(PropTypes.string),
   onSelect: PropTypes.func.isRequired,
   currentQuantity: PropTypes.number,
