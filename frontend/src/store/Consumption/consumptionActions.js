0e26a6ee48989fb36f819fc85f6e48457eb56541
Loic Baron <loic.baron@gmail.com>
2021-01-19 12:54:21 +0000

    refactor(consumption): handle selected and result

--- frontend/src/store/Consumption/consumptionActions.js
+++ frontend/src/store/Consumption/consumptionActions.js
@@ -1,6 +1,7 @@
 export const SELECT_PORTION = 'SELECT_PORTION';
 export const RESET_PORTION = 'RESET_PORTION';
 export const RESET_ALL_SELECTED_PORTIONS = 'RESET_ALL_SELECTED_PORTIONS';
+export const ADD_SELECTED_PORTIONS = 'ADD_SELECTED_PORTIONS';
 
 export const selectPortion = (item, position, age) => ({
   type: SELECT_PORTION,
@@ -12,4 +13,7 @@ export const resetPortion = item => ({
 });
 export const resetAllPortions = () => ({
   type: RESET_ALL_SELECTED_PORTIONS,
+});
+export const addSelectedPortions = () => ({
+  type: ADD_SELECTED_PORTIONS,
 });
--- frontend/src/store/Consumption/consumptionReducers.js
+++ frontend/src/store/Consumption/consumptionReducers.js
@@ -1,40 +1,60 @@
-import { SELECT_PORTION, RESET_PORTION, RESET_ALL_SELECTED_PORTIONS } from './consumptionActions';
+import { SELECT_PORTION, RESET_PORTION, RESET_ALL_SELECTED_PORTIONS, ADD_SELECTED_PORTIONS } from './consumptionActions';
 
-const result = [];
+const initialState = {
+  selected: {},
+  result: {}
+}
 
-function consumption(state = {}, action) {
+function consumption(state = initialState, action) {
   switch (action.type) {
     case SELECT_PORTION:
       // eslint-disable-next-line no-case-declarations
       const { item, position, age } = action.payload;
-      console.log('item', item);
-      console.log('position', position);
-      console.log('age', age);
       // eslint-disable-next-line no-case-declarations
       const { weight } = item.portions
         .find(portion => portion.position === position && portion.type === age) || 0;
-      console.log(weight);
-      result.push(
-        {
-          id: item.id,
-          name: item.name,
-          weight,
-        },
-      );
-      console.log(result);
       return {
         ...state,
-        [item.id]: position,
-        result: result,
+        selected: {
+          ...state.selected,
+          [item.id]: {
+            name: item.name,
+            weight,
+            position,
+          },
+        },
       };
     case RESET_PORTION:
       // eslint-disable-next-line no-case-declarations
       const key = action.payload.item.id;
       // eslint-disable-next-line no-case-declarations
-      const { [key]: value, ...withoutKey } = state;
-      return withoutKey;
+      const { [key]: value, ...withoutKey } = state.selected;
+      return {
+        ...state,
+        selected: withoutKey,
+      };
     case RESET_ALL_SELECTED_PORTIONS:
-      return { result };
+      return {
+        ...state,
+        selected: {},
+      };
+    case ADD_SELECTED_PORTIONS:
+      const portions = {};
+      for (const [id, item] of Object.entries(state.selected)) {
+        const currentWeight = state.result[id] ? state.result[id].weight : 0;
+        item.weight = item.weight ? item.weight : 0;
+        const totalWeight = currentWeight + item.weight;
+        if (totalWeight) {
+          portions[id] = {name: item.name, weight: currentWeight + item.weight};
+        }
+      }
+      return {
+        ...state,
+        result: {
+          ...state.result,
+          ...portions,
+        }
+      }
     default:
       return state;
   }
