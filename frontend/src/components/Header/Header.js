62273c07cb48470b4ca16e0ea26c50541bd9f463
Loic Baron <loic.baron@gmail.com>
2021-01-19 15:41:43 +0000

    feat(cart): add cart to Header

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -6,13 +6,22 @@ import { FormattedMessage } from 'react-intl';
 import RestaurantIcon from '@material-ui/icons/Restaurant';
 import './Header.css';
 
+import CartCustom from './CartCustom';
+
 const Header = () => (
   <AppBar>
     <Toolbar>
-      <a href="/" className="header-title">
-        <RestaurantIcon style={{ fontSize: 48, marginRight: 12 }} />
-        <Typography variant="h6"><FormattedMessage id="Header.title" /></Typography>
-      </a>
+      <div style={{display:'flex', width: '100%', justifyContent: 'space-between'}}>
+        <div>
+          <a href="/" className="header-title">
+            <RestaurantIcon style={{ fontSize: 48, marginRight: 12 }} />
+            <Typography variant="h6"><FormattedMessage id="Header.title" /></Typography>
+          </a>
+        </div>
+        <div style={{alignSelf:'center'}}>
+          <CartCustom />
+        </div>
+      </div>
     </Toolbar>
   </AppBar>
 );
