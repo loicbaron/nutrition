cf5cf2df78c62be6673cf9afa0e49078d95338a7
Loic Baron <loic.baron@gmail.com>
2021-03-02 12:06:23 +0000

    refactor AddItems

--- frontend/src/containers/AddItemsContainer.js
+++ frontend/src/containers/AddItemsContainer.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { injectIntl } from 'react-intl';
+import { connect } from 'react-redux';
 import { Fab } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import AddIcon from '@material-ui/icons/Add';
+
 import TooltipControlled from '../components/Navigation/TooltipControlled';
-import { connect } from 'react-redux';
 import { addSelectedPortions, resetAllPortions } from '../store/Consumption/consumptionActions';
 
 const styles = theme => ({
@@ -14,26 +15,31 @@ const styles = theme => ({
     right: theme.spacing(2),
   },
 });
-const AddItems = ({intl, classes, consumption, addSelectedPortions, resetAllPortions}) => {
-    if (Object.keys(consumption.selected).length > 0) {
-      return (
-          <TooltipControlled
-              title={intl.formatMessage({ id: "add.items.title" })}
-              text={intl.formatMessage({ id: "add.items.text" })}
-          >
-              <Fab color="secondary" aria-label="add" className={classes.fabButton}
-              onClick={() => { addSelectedPortions(); resetAllPortions(); }}>
-              <AddIcon />
-              </Fab>
-          </TooltipControlled>
-      );
-    } else {
-      return (
-        <Fab disabled aria-label="add" className={classes.fabButton}>
+const AddItems = ({
+  intl, classes, consumption, addSelectedPortions, resetAllPortions,
+}) => {
+  if (Object.keys(consumption.selected).length > 0) {
+    return (
+      <TooltipControlled
+        title={intl.formatMessage({ id: 'add.items.title' })}
+        text={intl.formatMessage({ id: 'add.items.text' })}
+      >
+        <Fab
+          color="secondary"
+          aria-label="add"
+          className={classes.fabButton}
+          onClick={() => { addSelectedPortions(); resetAllPortions(); }}
+        >
           <AddIcon />
         </Fab>
-      );
-    }
+      </TooltipControlled>
+    );
+  }
+  return (
+    <Fab disabled aria-label="add" className={classes.fabButton}>
+      <AddIcon />
+    </Fab>
+  );
 };
 
 
