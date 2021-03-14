0568b613a5ad5e453b8b2cf883a742f72b882aa2
Loic Baron <loic.baron@gmail.com>
2021-03-14 23:59:22 +0000

    build(intl): update react-intl

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import { IntlProvider, addLocaleData } from 'react-intl';
-import intlEN from 'react-intl/locale-data/en';
-import intlFR from 'react-intl/locale-data/fr';
+import { IntlProvider } from 'react-intl';
 import en from './translations/en.json';
 import fr from './translations/fr.json';
 import Page from './components/Layout/Page';
 import AppRouter from './AppRouter';
 import config from './configs/config';
-
-addLocaleData([...intlEN, ...intlFR]);
 
 const messages = {
   en,
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { addLocaleData } from 'react-intl';
-import localeEn from 'react-intl/locale-data/en';
 import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store';
 
-addLocaleData([...localeEn]);
 ReactDOM.render(
   <Provider store={store}>
     <App />
